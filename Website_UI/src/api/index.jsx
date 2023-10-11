47f13a4400b499fed9570320229821fc944d77cb
Daniel Mawalla <mawalladaniel2018@gmail.com>
2023-10-11 11:06:38 +0300

    first commit

--- Website_UI/src/api/index.jsx
+++ Website_UI/src/api/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const dev = true
+const dev = false
 export const backend_url = dev ? 'http://localhost:4000': 'https://api.chatafisha.com'
 export default axios.create({
   baseURL: dev ? 'http://localhost:4000/api/': 'https://api.chatafisha.com/api/',
